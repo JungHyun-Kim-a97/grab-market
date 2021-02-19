ab9bc24e5d58ed55f759717ce0c3a94e53d9119e
JungHyun-Kim-a97 <akdlswjf2@naver.com>
2021-02-19 16:45:51 +0900

    antd 기본적용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
+import "antd/dist/antd.css";
 import logo from "./logo.svg";
 import "./App.css";
 import MainPageComponent from "./main/index.js";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const history = useHistory();
   return (
     <div>
-      <Switch>
-        <Route exact={true} path="/">
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path="/products/:id">
-          <ProductPage />
-        </Route>
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            {/*기본 경로로 가기 위해 / 표시*/}
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+              {
+                /*push는 다른 경로로 이동하는 명령어*/
+              }
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,47 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams(); //해당 product페이지에는 id가 들어감
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://d1a1b399-04d4-44bf-a91a-12823e4dfcec.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  /*
+if문을 써주는 이유는 null에서 이미지 Url 인자를 가져올려고 하기 때문에
+방어 코드를 적어줘야함 (axios통신이 비동기 처리기 때문에 비동기 호출이 필요함, return은 함수를 종료 시키기 때문에 아래의 return문이 처리되는 걸 막아줌)
+*/
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
 
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box"></div>
+      <div id="name">{product.name}</div>
+      <div id="price">{product.price}원</div>
+      <div id="createdAt">2021년 02월 10일</div>
+      <div id="description">{product.description}</div>
+    </div>
+  );
 }
 
 export default ProductPage;
